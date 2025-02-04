7292331daa50c0ef9c8f32230e567c6c6d82fb6c
Robert Joseph Saltos <Rsaltos@csustan.edu>
2025-02-04 12:00:59 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,9 +9,9 @@ import numpy as np
 siteName = "Turlock CA USA"
 SampleRate = "1h"
 st.header = "Turlock AOD"
-StartDate = st.date_input("StartDate", datetime.date(2024, 1, 1))
+StartDate = st.date_input("StartDate", datetime.date(1, 1, 2024))
 StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
-EndDate = st.date_input("EndDate", datetime.date(2024, 1, 7))
+EndDate = st.date_input("EndDate", datetime.date(1,7, 2024))
 EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23, 59))
 
 # Allow the user to set y-axis limits
@@ -26,7 +26,7 @@ file_url_1 = "https://raw.githubusercontent.com/Rsaltos7/AODDATA/refs/heads/main
 def load_data(file_url):
     try:
         # Read the data from the provided GitHub raw URL
-        df = pd.read_csv(file_url, skiprows=6, parse_dates={'datetime': [0, 1]})
+        df = pd.read_csv(file_url, skiprows=7, parse_dates={'datetime': [0, 1]})
         datetime_utc = pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
         datetime_pac = pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
         df.set_index(datetime_pac, inplace=True)
