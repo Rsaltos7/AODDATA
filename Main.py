075f01970c0050d542a2d14f4ed8d099db573732
Robert Joseph Saltos <Rsaltos@csustan.edu>
2025-02-06 21:45:00 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ import numpy as np
 # Set up basic information
 siteName = "Modesto CA USA"
 SampleRate = "1h"
-st.header("Turlock AOD")  # Fixed this line
+st.header("Modesto AOD")  # Fixed this line
 StartDate = st.date_input("StartDate", datetime.date(2023, 7, 1))
 StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
 EndDate = st.date_input("EndDate", datetime.date(2023, 7, 7))
